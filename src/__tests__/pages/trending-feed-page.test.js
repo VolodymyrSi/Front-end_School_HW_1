a2302c87fa7378d8bf55a4fcde47eb33fc07f3e9
volodymyrSi <volodymyr.sinevych@gmail.com>
2022-01-31 20:43:07 +0200

    update

---
+++ src/__tests__/pages/trending-feed-page.test.js
@@ -0,0 +1,26 @@
+import Enzyme, { mount, shallow } from "enzyme";
+import TrendingFeedPage from "../../pages/TrendingFeedPage";
+import Adapter from "enzyme-adapter-react-16";
+import { TikTukContext } from "../../store/context";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+const contextValues = {isLoading: true, setIsLoading: jest.fn()}
+
+const setUp = () =>
+  shallow(
+    <TikTukContext.Provider value={contextValues}>
+      <TrendingFeedPage />
+    </TikTukContext.Provider>
+  );
+
+describe("`Trending feed component tests", () => {
+  describe("renders a div", () => {
+    const component = setUp();
+    console.log(component.debug());
+    it("should render a trending feed page component", () => {
+      const feedItem = component.find("TrendingFeedPage");
+      expect(feedItem).toHaveLength(1);
+    });
+  });
+});
