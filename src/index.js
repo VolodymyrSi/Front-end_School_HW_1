2bdebd2ab76c47bcdc00a79834726ea70c19eb40
volodymyrSi <volodymyr.sinevych@gmail.com>
2022-02-17 20:48:38 +0200

    github pages optimized

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./assets/styles/index.css";
 import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.querySelector("#root")
 );
